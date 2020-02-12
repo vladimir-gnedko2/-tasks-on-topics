d17c97e0ef29c4a8f9cf22f83a0e073a7f1b4221
Vladimir-Gnedko <vavanchikmks@gmail.com>
2020-02-12 21:54:00 +0300

    while/ for

---
+++ task8.js
@@ -0,0 +1,14 @@
+/* 'use strict'
+let x = 0;
+for(let i = 0; i <= 100; i++){
+    x += i;
+}
+console.log(x);
+ */
+let x = 0;
+let i = 0;
+while ( x <= 100 ) {
+    i += x;
+    x++;
+} 
+console.log (i);

f9058dc55aad26f08d189377737b2d3f166c04b0
Vladimir-Gnedko <vavanchikmks@gmail.com>
2020-03-10 21:56:16 +0300

    number

---
+++ done/number.js
@@ -0,0 +1,29 @@
+/* let num = 3;
+console.log(num);
+ */
+
+
+
+ /* let a = 10;
+ let b = 2;
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+  */
+
+
+  /* let c = 15;
+  let d = 2;
+  let result = c+d;
+  console.log(result);
+   */
+
+
+    let a = 17;
+    let b = 10;
+    let c = a-b;
+    let d = 7;
+    let result = c+d;
+    console.log(result);
+    
